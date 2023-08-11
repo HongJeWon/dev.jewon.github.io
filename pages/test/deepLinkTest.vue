c907e0b566aeae9e7478f2bcc65689fe897ae1ba
dev_jewon <dev.jewon@gmail.com>
2023-08-11 15:26:26 +0900

    딥링크 테스트 세팅

--- pages/test/deepLinkTest.vue
+++ pages/test/deepLinkTest.vue
@@ -3,7 +3,9 @@
 onMounted(() => {
   redirect();
 })
-let appURL = 'testapp://start';
+// let appURL = 'testapp://start';
+// let appURL = 'intent://start/#Intent;scheme=testapp;package=com.example.mytestapp;end';
+let appURL = 'intent://main?dest=/#Intent;scheme=fitamin;package=kr.fitamin;end';
 const redirect = () => {
 
   // 앱이 설치되어 있지 않을 경우 Google Play Store URL
@@ -11,12 +13,13 @@ const redirect = () => {
 
   // 앱을 시도하여 열기
   window.location.href = appURL;
-  setTimeout(() => {
-    if (!document.hasFocus()) {
-      return;
-    }
-    window.location.href = storeURL;
-  }, 5000);
+
+  // setTimeout(() => {
+  //   if (!document.hasFocus()) {
+  //     return;
+  //   }
+  //   window.location.href = storeURL;
+  // }, 5000);
 
 };
 const startApp = () => {
